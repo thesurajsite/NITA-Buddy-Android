1e52a570105389357a06c0a4d021b057ee8bd1bd
Suraj Verma <thesurajsite@gmail.com>
2025-06-17 22:07:50 +0530

    gradle and dependency updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.gharaana.nitabuddy"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.gharaana.nitabuddy"
@@ -41,24 +41,24 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.activity:activity:1.9.0")
+    implementation("androidx.core:core-ktx:1.16.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
+    implementation("androidx.activity:activity:1.10.1")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
 
     //GSON Converter
-    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.11.0")
 
     //Volley
     implementation("com.android.volley:volley:1.2.1")
 
     //sdp
-    implementation ("com.intuit.sdp:sdp-android:1.1.0")
+    implementation ("com.intuit.sdp:sdp-android:1.1.1")
 
     // swipe refresh
     implementation ("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
@@ -69,7 +69,7 @@ dependencies {
 
     // Retrofit
     implementation("com.squareup.retrofit2:retrofit:3.0.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.1")
 
     implementation("org.jetbrains.kotlin:kotlin-stdlib:2.1.21")
 
